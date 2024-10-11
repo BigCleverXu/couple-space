dc8d2d18fae6c1e000922f1c14de787bb87f4ade
xuyijie_xu <13989569775@163.com>
2024-10-11 20:47:30 +0800

    feat: 姨妈助手

---
+++ miniprogram/components/date-picker/index.js
@@ -0,0 +1,65 @@
+// components/date-picker/index.js
+import {
+  days,
+  months
+} from './data'
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    hasMonth: {
+      type: Boolean,
+      value: true
+    },
+    hasDay: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    month: months,
+    day: days,
+    visible: false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onColumnChange(e) {
+      console.log('picker pick:', e);
+    },
+
+    onPickerChange(e) {
+      const {
+        value
+      } = e.detail;
+
+      console.log('picker change:', e.detail);
+      this.triggerEvent('change', e.detail)
+      this.setData({
+        visible: false,
+        value: value,
+        text: value.join(' '),
+      });
+    },
+
+    onPickerCancel(e) {
+      console.log(e, '取消');
+      this.setData({
+        visible: false,
+      });
+    },
+    onSeasonPicker() {
+      this.setData({
+        visible: true
+      });
+    },
+  }
+})
--- miniprogram/pages/assistant/index.js
+++ miniprogram/pages/assistant/index.js
@@ -5,14 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    formData: {}
+    formData: {},
+
+  },
+  change(e) {
+    console.log(e.detail);
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
     setTimeout(() => {
-      
+
     }, 500)
   },
 
